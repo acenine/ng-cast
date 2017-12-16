ab84259e42a245b2a01b15bb49db8759910cf053
acenine <acenine@gmx.com>
2017-12-15 16:13:56 -0800

    Complete through passing exampleData to videoList

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,11 @@
 angular.module('video-player')
 .component('videoList', {
   // TODO
+  bindings: {
+    videoData: '<'
+  },
+  controller: () => {
+
+  },
+  templateUrl: '/src/templates/videoList.html'
 });
